17223ce59ea125c691540d8a90cf63e687b24922
jyotibhambri <fnujyoti1984@gmail.com>
2024-02-21 10:16:30 -0600

    folder structure

--- template.py
+++ template.py
@@ -37,3 +37,14 @@ list_of_files = [
 
 
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
